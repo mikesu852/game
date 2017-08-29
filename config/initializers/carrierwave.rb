bf5e38aaa02a66cc11182501e7c3a7ec2f5c2677
Mike <mike@swat.biz>
2017-08-30 02:12:17 +0800

    準備 deploy to ubuntu

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,15 @@
 # config.validate_unique_filename = true
 #   config.validate_filename_format = true
 #   config.validate_remote_net_url_format = false # this is important
+
+CarrierWave.configure do |config|
+config.fog_credentials = {
+
+    :provider                         => 'Google',
+    :google_storage_access_key_id     => Rails.application.secrets.google_storage_access_key_id,
+    :google_storage_secret_access_key => Rails.application.secrets.google_storage_secret_access_key
+
+    }
+
+    config.fog_directory = 'tainangame'
+end
